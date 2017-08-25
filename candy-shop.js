21f02d710b3b15db3d56b8fdc4b4e36ef0d639aa
Josh Bowling <joshuaebowling@gmail.com>
2017-08-25 11:02:44 -0400

    decoupled logic from viewmodel
    moved data + logic to postal-based services

--- candy-shop.js
+++ candy-shop.js
@@ -1,104 +1,144 @@
-var data = {
-    title: 'Welcome to the Candy Shop',
-    
-    products: [
-      {
-        title: 'lollipop',
-        price: 0.5
-      },
-      {
-        title: 'teakettle',
-        price: 14.95
-      },
-      {
-        title: 'magic stick',
-        price: 89.12546
-      },
-      {
-        title: 'low rider',
-        price: 22450
-      },
-      {
-        title: 'champagne',
-        price: 45
-      }
-    ],
-    
+    var cartService, controller, data, productService;
+    cartService = (() => {
+        var cartChannel, cart, totals, updateTotals;
+        cart = {
+            store: [],
+            totals: {
+                subTotal: 0,
+                get tax() {
+                    return cart.totals.subTotal * .05;
+                },
+                get total() {
+                    return cart.totals.subTotal + cart.totals.tax;
+                }
+            }
+        };
+        cartChannel = postal.channel('cart');
+        // pickup any request for cart items
+        cartChannel.subscribe('get.request', (crit, env) => {
+            cartChannel.publish('get.response', store);
+        });
+        cartChannel.subscribe('add.request', (item, env) => {
+            var existentItem = _.find(cart.store, (cartitem) => { return item.id === cartitem.id });
+            if(existentItem) {
+                existentItem.quantity++;
+            } else {
+                item.quantity = 1;
+                cart.store.push(item);
+            }
+            updateTotals();
+            cartChannel.publish('add.response', cart);
+        });
+        cartChannel.subscribe('remove.request', (item, env) => {
+            var existentItem = _.find(cart.store, item);
+            if(existentItem && existentItem.quantity > 1) {
+                existentItem.quantity--;
+            } else {
+                _.remove(cart.store, item);
+            }
+            updateTotals();
+            cartChannel.publish('remove.response', cart);
+        });
+
+        updateTotals = function() {
+            var result = 0;
+            _.each(cart.store, (item) => result += (item.price * item.quantity));
+            cart.totals.subTotal = result;
+        };
+        return {
+            get: function(crit = {}) {
+                postal.channel('cart').publish('get.request', crit);
+            },
+            add: function(item) {
+                postal.channel('cart').publish('add.request', item);
+            },
+            remove: function(item) {
+                postal.channel('cart').publish('remove.request', item);
+            }
+        };
+    })();
+
+    productService = (() => {
+        var store =  _.map([
+                {
+                    title: 'teakettle',
+                    price: 14.95
+                },
+                {
+                    title: 'lollipop',
+                    price: 0.5
+                },
+                {
+                    title: 'magic stick',
+                    price: 89.12546
+                },
+                {
+                    title: 'low rider',
+                    price: 22450
+                },
+                {
+                    title: 'champagne',
+                    price: 45
+                }
+            ],
+        (item, i) => { 
+            item.id = i; 
+            return item;
+        });
+        // pickup any request for cart items
+        postal.channel('product').subscribe('get.request', (crit, env) => {
+            postal.channel('product').publish('get.response', store);
+        });
+
+        return {
+            get: function(crit = {}) {
+                postal.channel('product').publish('get.request', crit);
+            }
+        };
+    })();
+ data = {
+    title: 'Welcome',
+    products: [],
     bag: [],
     
     subtotal: 0,
     
     tax: 0,
     
-    total: 0
-  },
+    total: 0     
+ };
   
   controller = {
     onAtbClick: function(e, model) {
-      
-      var product = model.data.products[model.index],
-        bag = model.data.bag,
-        i = 0,
-        updatePrice = model.controller.updatePrice;
-      
-      for(; i < bag.length; i++) {
-        
-        if(bag[i].title === product.title) {
-          
-          bag[i].quantity++;
-          updatePrice(model.data);
-          return;
-        }
-      }
-      
-      bag.push(product);
-      bag[bag.length - 1].quantity = 1;
-      updatePrice(model.data);
+        cartService.add(model.data.products[model.index]);
     },
     
     addItem: function(e, model) {
-      
-      model.data.bag[model.index].quantity++;
-      model.controller.updatePrice(model.data);
+      cartService.add(model.data.bag[model.index]);
     },
-    
+    getActualModel: function(model) {
+        return model.data.products[model.index];
+    },
     removeItem: function(e, model) {
-      
-      var index = model.index,
-        bag = model.data.bag,
-        product = bag[index],
-        updatePrice = model.controller.updatePrice;
-      
-      if(product.quantity > 1) {
-        
-        product.quantity--;
-        updatePrice(model.data);
-        return;
-      }
-      
-      bag.splice(index, 1);
-      updatePrice(model.data);
-    },
-    
-    updatePrice: function(data) {
-      
-      var bag = data.bag,
-        product,
-        subtotal = 0,
-        i = 0;
-      
-      for(; i < bag.length; i++) {
-        
-        product = bag[i];
-        
-        subtotal += product.price * product.quantity;
-      }
-      
-      data.subtotal = subtotal;
-      data.tax = subtotal * 0.08875;
-      data.total = subtotal + data.tax;
+        cartService.remove(model.data.bag[model.index]);
     }
+    
   };
+
+// subscriptions for controller
+postal.channel('cart').subscribe('*.response', (cart, env) => {
+    // clear it
+    data.bag.splice(0, data.bag.length);
+   // repopulate without changing the reference -- rivets won't pickup on it changes like data.bag = x, you have to repopulate the original array
+   _.each(cart.store, (item) => data.bag.push(item));
+   // same thing here, replace each value
+   data.subtotal = cart.totals.subTotal;
+   data.tax = cart.totals.tax;
+   data.total = cart.totals.total;
+});
+
+
+
 
 rivets.formatters.price = function(val) {
   
@@ -143,8 +183,16 @@ rivets.formatters.length = function(val) {
   
   return val.length;
 }
+// listen for when the product service returns a list of products, will also work for paging
+postal.channel('product').subscribe('get.response', (d, env) => {
+    _.each(_.uniq(d, data.products), (p, pi) => {
+        data.products.push(p);
+    });
+});
 
-rivets.bind(document.querySelector('#candy-shop'), {
+productService.get({});
+rivets.bind($('#candy-shop'), {
   data: data,
   controller: controller
 });
+
