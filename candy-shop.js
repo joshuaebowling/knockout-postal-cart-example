095d7a22eb1b6a19d6570244a4971343a1c3ea9e
Josh Bowling <joshuaebowling@gmail.com>
2017-09-15 21:47:42 -0400

    moved static data out of productService, added CertificationService, factored out some es5 in favor of es6, declared viewmodel for CertificationFilter

--- candy-shop.js
+++ candy-shop.js
@@ -1,9 +1,152 @@
 var 
+productData, 
 // services
-cartService, productService, messageService,
+cartService, certificationService, productService, messageService,
 //view models
-BagItemModel, BagModel, CartModel, FreeShippingModel, ItemsInCartModel, MessageModel,
+BagItemModel, BagModel, CartModel, CertificationFilterViewModel, FreeShippingModel, ItemsInCartModel, MessageModel,
     ProductModel, ProductsModel, ProductsModelNavigation;
+
+productData = [
+    {
+        title: 'Coffee',
+        price: 14.95,
+        available: 27,
+        discountAfter: 6,
+        certifications: ['USDA Organic', 'GMO Free', 'Fair Trade'],
+        tags: ['Origin:Costa Rica', 'Descriptor:Light Roast', 'Flavor: Mild & Nutty'],
+        rating: 10
+    },
+    {
+        title: 'fruit leather',
+        price: 0.5,
+        available: 50,
+        discountAfter: 6,
+        certifications: ['USDA Organic', 'GMO Free'],
+        tags: ['Origin:Costa Rica', 'Descriptor:Light Roast', 'Flavor: Mild & Nutty'],
+        rating: 9
+    },
+    {
+        title: 'mid-level fancy wine',
+        price: 89.12546,
+        available: 13,
+        discountAfter: 6,
+        certifications: ['GMO Free'],
+        tags: ['Origin:Italy', 'Grape:Chianti', 'Flavor: Floral + Smooth Finish'],
+        rating: 6
+    },
+    {
+        title: 'moon round trip ticket',
+        price: 22450,
+        available: 4,
+        discountAfter: 6,
+        certifications: ['GMO Free'],
+        tags: ['Departs:Tomorrow', 'Returns:LOL, BC', 'Seating:Business Class'],
+        rating: null
+    },
+    {
+        title: 'champagne',
+        price: 45,
+        available: 7,
+        discountAfter: 6,
+        certifications: ['GMO Free'],
+        tags: ['Origin:France', 'Grape:Buena Pregunta', 'Flavor: Bubbly'],
+        rating: 6
+        
+    },
+    {
+        title: 'Pea Protein Powder',
+        price: 14.95,
+        available: 23,
+        discountAfter: 6,
+        certifications: ['USDA Organic', 'GMO Free'],
+        tags: ['Buzz: Branched-Chain Amino Acids', 'Flavor:Meh', 'Recommender: Dr Feelgo Ood'],
+        rating: 8
+    },
+    {
+        title: 'Egg',
+        price: 0.5,
+        available: 500,
+        discountAfter: 12,
+        certifications: ['USDA Organic', 'GMO Free', 'Free Range'],
+        tags: ['Origin:Local', 'Flavor: Chickeny'],
+        rating: 9
+    },
+    {
+        title: 'Ribeye',
+        price: 50.12546,
+        available: 13,
+        discountAfter: null,
+        certifications: ['UDSA Organic', 'USDA Prime'],
+        tags: ['Origin:Texas', 'Average Weight: 1 lb', 'Feed:Grass'],
+        rating: 9
+    },
+    {
+        title: 'Total Recall',
+        price: 27450,
+        available: NaN,
+        discountAfter: null,
+        certifications: ['APA'],
+        tags: ['Name:Quaid', 'Muscles:Big', 'Seating:Mostly Running'],
+        rating: 4
+    },
+    {
+        title: 'Men\'s Multivitamin',
+        price: 45,
+        available: 18,
+        discountAfter: 6,
+        certifications: ['GMO Free', 'USDA Organic', 'Whole Food'],
+        tags: ['Raw', 'Vegan', '44 Superfoods'],
+        rating: 6
+        
+    },
+    {
+        title: 'Rye Berries',
+        price: 14.95,
+        available: 27,
+        discountAfter: 6,
+        certifications: ['USDA Organic', 'GMO Free', 'Fair Trade'],
+        tags: ['Amount:1lb', 'Awesome Use:Mycology'],
+        rating: 10
+    },
+    {
+        title: 'Ketchup',
+        price: 5,
+        available: 24,
+        discountAfter: 6,
+        certifications: ['USDA Organic', 'GMO Free'],
+        tags: ['Roma Tomatoes', 'Amount:8 oz', 'Color:Green'],
+        rating: 9
+    },
+    {
+        title: 'Cheddar Hoop',
+        price: 87.12546,
+        available: 13,
+        discountAfter: null,
+        certifications: ['GMO Free', 'USDA Organic'],
+        tags: ['Color:White', 'Origin:Wisconsin', 'Flavor:Sharp'],
+        rating: 3
+    },
+    {
+        title: 'Journey to Center of Earth in Time Machine',
+        price: 22450,
+        available: 3,
+        discountAfter: 6,
+        certifications: null,
+        tags: ['Departs:Yesterday', 'Returns:Do you have to ask?', 'NO AC'],
+        rating: null
+    },
+    {
+        title: 'Pine Nuts',
+        price: 17,
+        available: 14,
+        discountAfter: 6,
+        certifications: ['GMO Free', 'USDA Organic'],
+        tags: ['Origin:Washington State', 'Awesome Use: Hummus'],
+        rating: 7
+        
+    }
+    
+];
 
 cartService = (() => {
     var adjustQuantity, cart, channel, totals, updateTotals;
@@ -82,147 +225,7 @@ productService = (() => {
     const channel = postal.channel('product');
     var adjustAvailable, currentPage, critDefaults, page, store;
     
-    store =  _.map([
-            {
-                title: 'Coffee',
-                price: 14.95,
-                available: 27,
-                discountAfter: 6,
-                certifications: ['USDA Organic', 'GMO Free', 'Fair Trade'],
-                tags: ['Origin:Costa Rica', 'Descriptor:Light Roast', 'Flavor: Mild & Nutty'],
-                rating: 10
-            },
-            {
-                title: 'fruit leather',
-                price: 0.5,
-                available: 50,
-                discountAfter: 6,
-                certifications: ['USDA Organic', 'GMO Free'],
-                tags: ['Origin:Costa Rica', 'Descriptor:Light Roast', 'Flavor: Mild & Nutty'],
-                rating: 9
-            },
-            {
-                title: 'mid-level fancy wine',
-                price: 89.12546,
-                available: 13,
-                discountAfter: 6,
-                certifications: ['GMO Free'],
-                tags: ['Origin:Italy', 'Grape:Chianti', 'Flavor: Floral + Smooth Finish'],
-                rating: 6
-            },
-            {
-                title: 'moon round trip ticket',
-                price: 22450,
-                available: 4,
-                discountAfter: 6,
-                certifications: ['GMO Free'],
-                tags: ['Departs:Tomorrow', 'Returns:LOL, BC', 'Seating:Business Class'],
-                rating: null
-            },
-            {
-                title: 'champagne',
-                price: 45,
-                available: 7,
-                discountAfter: 6,
-                certifications: ['GMO Free'],
-                tags: ['Origin:France', 'Grape:Buena Pregunta', 'Flavor: Bubbly'],
-                rating: 6
-                
-            },
-            {
-                title: 'Pea Protein Powder',
-                price: 14.95,
-                available: 23,
-                discountAfter: 6,
-                certifications: ['USDA Organic', 'GMO Free'],
-                tags: ['Buzz: Branched-Chain Amino Acids', 'Flavor:Meh', 'Recommender: Dr Feelgo Ood'],
-                rating: 8
-            },
-            {
-                title: 'Egg',
-                price: 0.5,
-                available: 500,
-                discountAfter: 12,
-                certifications: ['USDA Organic', 'GMO Free', 'Free Range'],
-                tags: ['Origin:Local', 'Flavor: Chickeny'],
-                rating: 9
-            },
-            {
-                title: 'Ribeye',
-                price: 50.12546,
-                available: 13,
-                discountAfter: null,
-                certifications: ['UDSA Organic', 'USDA Prime'],
-                tags: ['Origin:Texas', 'Average Weight: 1 lb', 'Feed:Grass'],
-                rating: 9
-            },
-            {
-                title: 'Total Recall',
-                price: 27450,
-                available: NaN,
-                discountAfter: null,
-                certifications: ['APA'],
-                tags: ['Name:Quaid', 'Muscles:Big', 'Seating:Mostly Running'],
-                rating: 4
-            },
-            {
-                title: 'Men\'s Multivitamin',
-                price: 45,
-                available: 18,
-                discountAfter: 6,
-                certifications: ['GMO Free', 'USDA Organic', 'Whole Food'],
-                tags: ['Raw', 'Vegan', '44 Superfoods'],
-                rating: 6
-                
-            },
-            {
-                title: 'Rye Berries',
-                price: 14.95,
-                available: 27,
-                discountAfter: 6,
-                certifications: ['USDA Organic', 'GMO Free', 'Fair Trade'],
-                tags: ['Amount:1lb', 'Awesome Use:Mycology'],
-                rating: 10
-            },
-            {
-                title: 'Ketchup',
-                price: 5,
-                available: 24,
-                discountAfter: 6,
-                certifications: ['USDA Organic', 'GMO Free'],
-                tags: ['Roma Tomatoes', 'Amount:8 oz', 'Color:Green'],
-                rating: 9
-            },
-            {
-                title: 'Cheddar Hoop',
-                price: 87.12546,
-                available: 13,
-                discountAfter: null,
-                certifications: ['GMO Free', 'USDA Organic'],
-                tags: ['Color:White', 'Origin:Wisconsin', 'Flavor:Sharp'],
-                rating: 3
-            },
-            {
-                title: 'Journey to Center of Earth in Time Machine',
-                price: 22450,
-                available: 3,
-                discountAfter: 6,
-                certifications: null,
-                tags: ['Departs:Yesterday', 'Returns:Do you have to ask?', 'NO AC'],
-                rating: null
-            },
-            {
-                title: 'Pine Nuts',
-                price: 17,
-                available: 14,
-                discountAfter: 6,
-                certifications: ['GMO Free', 'USDA Organic'],
-                tags: ['Origin:Washington State', 'Awesome Use: Hummus'],
-                rating: 7
-                
-            }
-            
-        ], // keep it at 5 until paging is complete
+    store =  _.map(productData, // keep it at 5 until paging is complete
     (item, i) => { 
         item.id = i;    
         return item;
@@ -266,7 +269,6 @@ productService = (() => {
         var found, quantity;
         // an increase in cart is a decrease in product and vice-versa, so flip the value
         adjustAvailable(data.changed, -(data.quantity));
-        console.log(data.changed.available);
         if(data.changed.available === 0) messageService.display(messageService.constants.messageTypes.alert, "You bought us out!!!");         
         
         // if the updated item is in the currentPage, publish the change
@@ -306,6 +308,28 @@ messageService = (() => {
 
     }
 })();
+
+certificationService = (() => {
+    var channel;
+    channel = postal.channel('certification');
+    channel.subscribe('get.request', (message, env) => {
+        var result;
+        result = _.chain(productData).map(p => p.certifications).flatten().uniq().value();
+        console.log(result);
+        channel.publish('get.response', result);
+    });
+    return {
+        // queue would be be good here
+        get: () => 
+            channel.publish('get.request'),
+        subscriptions: {
+            getResponse: (todo) =>
+                channel.subscribe('get.response', todo)
+        }
+
+    }
+})();
+
 
 CartModel = function(attributes) {
     var vm;
@@ -357,12 +381,10 @@ BagItemModel = function(context) {
 
     vm = {
         model: context.model,
-        addItem: function() {
-          cartService.change(vm.model, 1);
-        },
-        removeItem: function() {
-          cartService.change(vm.model, -1);
-        } 
+        addItem: () =>
+          cartService.change(vm.model, 1),
+        removeItem: () =>
+          cartService.change(vm.model, -1)
     };
 
     return vm;      
@@ -454,12 +476,11 @@ ProductsModelNavigation = function() {
     };
 
     vm.skip = ko.computed(() => vm.currentPage() * vm.pageSize);
-    vm.nextPage = function() {
+    vm.nextPage = () =>
         productService.get({ limit: vm.pageSize, skip: vm.skip() });
-    };
-    vm.prevPage = function() {
+    vm.prevPage = () =>
         productService.get({ limit: vm.pageSize, skip: vm.skip() - ( vm.pageSize * 2 ) });
-    };
+
 
     postal.channel('product').subscribe('get.response', (d, env) => {
         vm.currentPage(d.skip/vm.pageSize);
@@ -475,20 +496,14 @@ ProductModel = function(context) {
 
     vm = {
         model: context.model,        
-        addToBag: function() {
-            cartService.change(context.model, 1);
-        }
+        addToBag: () =>
+            cartService.change(context.model, 1)
     };
     return vm;        
 };
 
-
-
-
-
-
 // rivets.formatters.price = function(val) {
-  
+
 //   var spl = String(val).split('.'),
 //     dollarsArray = spl[0].split(''),
 //     lastDollar = dollarsArray.length - 1,
