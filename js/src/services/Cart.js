8003bcb086c70ce8e7a8690e7a25b6defab671ea
Josh Bowling <joshuaebowling@gmail.com>
2017-09-17 22:59:32 -0400

    added lodash and postal dependencies, checked cartservice for syntax error applies to #19

--- js/src/services/Cart.js
+++ js/src/services/Cart.js
@@ -1,5 +1,8 @@
+const 
+    postal = require('postal'),
+    _ = require('lodash'),
 // this will be a contstructor, but in the index it will be a singleton
-const CartService = function ctor_CartService() {
+CartService = function ctor_CartService() {
     var adjustQuantity, cart, channel, totals, updateTotals;
     channel = postal.channel('cart');
     cart = {
@@ -21,6 +24,55 @@ const CartService = function ctor_CartService() {
             }
         }
     };
+
+    adjustQuantity = function CartService_adjustQuantity(item, quantity) {
+        var existentItem = _.find(cart.store, (cartitem) => { return item.id === cartitem.id });
+        if(existentItem) {
+            existentItem.quantity += quantity;
+            if(existentItem.quantity === 0) _.remove(cart.store, existentItem);
+        } else {
+            item.quantity = quantity;
+            cart.store.push(item);
+            // now the item is existent
+            existentItem = item;
+        }
+        updateTotals();
+        return existentItem;
+    };
+
+    updateTotals = function() {
+        var result = 0;
+        _.each(cart.store, (item) => result += (item.price * item.quantity));
+        cart.totals.subTotal = result;
+    };
+
+    // pickup any request for cart items
+    channel.subscribe('get.request', (crit, env) => {
+        channel.publish('get.response', { cart });
+    });
+
+    channel.subscribe('change.request', (request, env) => {
+        var changed;
+        
+        changed = adjustQuantity(request.item, request.quantity);
+        channel.publish('change.response', { cart, changed, quantity: request.quantity });
+    });
+    
+    return {
+        get: function(crit = {}) {
+            postal.channel('cart').publish('get.request', crit);
+        },
+        change: function(item, quantity) {
+            postal.channel('cart').publish('change.request', { item, quantity });
+        },
+        // not sure about this pattern
+        subscriptions: {
+            // lets see about currying this 
+            onChange: function(todo) { channel.subscribe('change.response', todo) },
+            anyResponse: function(todo) { channel.subscribe('*.response', todo) }
+        }
+    };
 };
+
 
 module.exports = CartService;
