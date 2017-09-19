6fdba7c4db2993d3dbc41c23101f2300353d11a6
Josh Bowling <joshuaebowling@gmail.com>
2017-09-18 23:26:46 -0400

    inital commit of CartViewModel applies to #11

---
+++ js/src/ko/viewmodels/Cart.js
@@ -0,0 +1,23 @@
+module.exports = function(attributes) {
+    var vm;
+
+    vm = {
+        attributes: attributes,
+        title: 'Welcome to the Postal Smart Cart',
+        subtotal: ko.observable(0),
+        tax: ko.observable(0),
+        total: ko.observable(0)
+    };
+    vm.show = ko.computed(() => { 
+        return vm.subtotal() > 0; 
+    });
+    
+    // subscriptions for controller
+    cartService.subscriptions.anyResponse((data, env) => {
+        // same thing here, replace each value
+        vm.subtotal(data.cart.totals.subTotal);
+        vm.tax(data.cart.totals.tax);
+        vm.total(data.cart.totals.total);
+    });
+    return vm;
+};
