45ff5cf8cead13a6c308e2b62690e9dabd59a147
Josh Bowling <joshuaebowling@gmail.com>
2017-09-18 23:49:47 -0400

    added index for ko viewmodels modules applies to #11

---
+++ js/src/ko/viewmodels/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+    App: require('./App'),
+    Cart: require('./Cart'),
+    CartItem: require('./CartItem'),
+    Certification: require('./Certification'),
+    FreeShipping: require('./FreeShipping'),
+    ItemsInCart: require('./ItemsInCart'),
+    Message: require('./Message'),
+    Product: require('./Product'),
+    Products: require('./Products'),
+    Product: require('./Product'),
+    ProductsNavigation: require('./ProductsNavigation')
+};
