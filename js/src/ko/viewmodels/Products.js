942f9dd20b4f75ff771453f67ec721bc468c794f
Josh Bowling <joshuaebowling@gmail.com>
2017-09-20 22:54:05 -0400

    updatd declarations to suit es6 syntax applies to #11

--- js/src/ko/viewmodels/Products.js
+++ js/src/ko/viewmodels/Products.js
@@ -2,16 +2,14 @@ const
     ko = require('ko'),
     _ = require('lodash');
 
-module.exports = function ProductsModel(attributes) {
-    var vm;
-
-    vm = {
+module.exports = function ProductsModel(services, attributes) {
+    const vm = {
         attributes: attributes,
         products: ko.observableArray([])
     };
 
     // listen for when the product service returns a list of products, will also work for paging 
-    productService.subscriptions.onGet((payload, env) => {
+    services.productService.subscriptions.onGet((payload, env) => {
         var products;
         vm.products.removeAll();
         _.each(payload.result.page, (product) => vm.products.push(product));
