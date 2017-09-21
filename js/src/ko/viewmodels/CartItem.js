639718a80c24dc9730139e2bf0dd224e7159ac25
Josh Bowling <joshuaebowling@gmail.com>
2017-09-20 22:47:39 -0400

    updatd declarations to suit es6 syntax applies to #11

--- js/src/ko/viewmodels/CartItem.js
+++ js/src/ko/viewmodels/CartItem.js
@@ -1,15 +1,12 @@
 const ko = require('ko');
 
 module.exports = function CartItemModel(services, context) {
-    var cartService, vm;
-
-    cartService = services.cartService;
-    vm = {
+    const vm = {
         model: context.model,
         addItem: () =>
-          cartService.change(vm.model, 1),
+          services.cartService.change(vm.model, 1),
         removeItem: () =>
-          cartService.change(vm.model, -1)
+          services.cartService.change(vm.model, -1)
     };
 
     return vm;
