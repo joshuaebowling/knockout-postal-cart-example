9176f68234dda4c3ca6eb04680df611268031697
Josh Bowling <joshuaebowling@gmail.com>
2017-09-20 22:46:49 -0400

    added service argument to model applies to #11

--- js/src/ko/viewmodels/Certifications.js
+++ js/src/ko/viewmodels/Certifications.js
@@ -2,20 +2,18 @@ const
     ko = require('ko'),
     _ = require('lodash');
 
-module.exports = function CertificationsModel(attributes) {
-    var vm;
-
-    vm = {
+module.exports = function CertificationsModel(services, attributes) {
+    const vm = {
         attributes: attributes,
         certifications: ko.observableArray([]),
         selectedCertifications: ko.observableArray([])
     };
 
     vm.selectedCertifications.subscribe((newVal, oldVal) => {
-        productService.get({ certifications: newVal });
+        services.productService.get({ certifications: newVal });
     });
     // listen for when the product service returns a list of products, will also work for paging 
-    certificationService.subscriptions.onGet((certifications, env) => {
+    services.certificationService.subscriptions.onGet((certifications, env) => {
         vm.certifications.removeAll();
         _.each(certifications, (certification) => vm.certifications.push(certification));
     });
